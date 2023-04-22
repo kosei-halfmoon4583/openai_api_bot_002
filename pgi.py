7fb5273a196782dac01b2f9df2cdc21469f123d0
Naoshi Watanuki <kosei-halfmoon4583@users.noreply.github.com>
2023-04-22 15:35:18 +0900

    Add files via upload

--- pgi.py
+++ pgi.py
@@ -1,7 +1,7 @@
 # 以下を「pgi.py」に書き込み
 import streamlit as st
 import openai
-import secret_keys  # 外部ファイルにAPI keyを保存
+# import secret_keys  # 外部ファイルにAPI keyを保存
 
 # openai.api_key = secret_keys.openai_api_key
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
