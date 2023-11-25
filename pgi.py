5599c77d056c04e605bc2c501ac40a0f0389ccef
Naoshi Watanuki <kosei-halfmoon4583@users.noreply.github.com>
2023-11-25 11:48:21 +0900

    Add files via upload

--- pgi.py
+++ pgi.py
@@ -20,13 +20,15 @@ def communicate():
     user_message = {"role": "user", "content": st.session_state["user_input"]}
     messages.append(user_message)
 
-    response = openai.ChatCompletion.create(
+#    response = openai.ChatCompletion.create(
+# 2023/11/25 update.
+    response = openai.chat.completions.create(
         model="gpt-3.5-turbo",
         messages=messages
     )
 
-    bot_message = response["choices"][0]["message"]
-# 2023/11/25 update 
+#    bot_message = response["choices"][0]["message"]
+# 2023/11/25 update
 #    bot_message = response.choices[0].message.content
     messages.append(bot_message)
 
